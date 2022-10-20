505504dd1f9df1326c062d4dd839b748c2e0a335
phalla.doll <phalla.doll@okoone.com>
2022-10-20 23:59:37 +0700

    WIP admin page

--- src/app/page/admin-page/admin-page.component.ts
+++ src/app/page/admin-page/admin-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FirestoreDbService} from "../../shared/firestore-db.service";
 
 @Component({
   selector: 'app-admin-page',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminPageComponent implements OnInit {
 
-  constructor() { }
+  products: any[] = [];
+  categories: any[] = [];
+  users: any[] = [];
+  isLoadingProducts: boolean = true;
+  isLoadingCategories: boolean = true;
+  isLoadingUsers: boolean = true;
+
+  constructor(
+    public _FirestoreDbService: FirestoreDbService,
+  ) { }
 
   ngOnInit(): void {
+    this.onLoadCategories().then();
+    this.onLoadProducts().then();
+    this.onLoadUsers().then();
+  }
+
+  async onLoadCategories() {
+    this.categories = await this._FirestoreDbService.getRecords('categories');
+    this.categories.sort((a, b) => (a.ordering > b.ordering) ? 1 : -1);
+    console.log('categories', this.categories);
+    this.isLoadingCategories = false;
+  }
+
+  async onLoadProducts() {
+    this.products = await this._FirestoreDbService.getRecords('products');
+    console.log('products', this.products);
+    this.isLoadingProducts = false;
+  }
+  async onLoadUsers() {
+    this.users = await this._FirestoreDbService.getRecords('users');
+    console.log('users', this.users);
+    this.isLoadingUsers = false;
   }
 
 }
